af097b6f178f9e3b987e36fb1ffb428561806684
mchl <mchlginga@gmail.com>
2025-07-05 02:37:16 +0800

    update server.js

--- notes-api/server.js
+++ notes-api/server.js
@@ -3,11 +3,18 @@ const app = express();
 const port = 3000;
 
 const notesRouter = require("./routes/notes.js");
+const logger = require("./middleware/logger.js");
 
-app.set('json spaces', 2);
+app.set("json spaces", 2);
+
 app.use(express.json());
+app.use(logger);
 
 app.use("/notes", notesRouter); // routes
+
+app.use( (req, res) => {
+    res.status(404).json({error: "Route not found"});
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
