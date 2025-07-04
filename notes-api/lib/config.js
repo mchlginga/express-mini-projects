5f2492524b8c849a23b8f1f028c3ed3a4b5a4feb
mchl <mchlginga@gmail.com>
2025-07-05 02:40:19 +0800

    config: add log file path for logger middleware

--- notes-api/lib/config.js
+++ notes-api/lib/config.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 const PATHS = {
     dataDir: path.join(__dirname, "..", "data"),
-    notesFile: path.join(__dirname, "..", "data", "notes.json")
+    notesFile: path.join(__dirname, "..", "data", "notes.json"),
+    logFile: path.join(__dirname, "..", "data", "logs.txt")
 };
 
 module.exports = {
