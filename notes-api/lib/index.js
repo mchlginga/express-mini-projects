20ca6998b6678559a1a6cc39abd40498cd630c3c
mchl <mchlginga@gmail.com>
2025-07-05 02:46:29 +0800

    feat: add lib index for centralized exports

---
+++ notes-api/lib/index.js
@@ -0,0 +1,11 @@
+const ensureNoteFile = require("./ensureNoteFile.js");
+const config = require("./config.js");
+const normalizeTitle = require("./normalizeTitle.js");
+const parseNotes = require("./parseNotes.js");
+
+module.exports = {
+    ensureNoteFile,
+    config,
+    normalizeTitle,
+    parseNotes
+};
