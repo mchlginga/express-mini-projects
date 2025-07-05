d15bd4d7c90e2ee2269b00b5e5c99cac11d464d8
mchl <mchlginga@gmail.com>
2025-07-05 17:45:10 +0800

    feat: add DELETE route for note deletion

--- notes-api/routes/notes.js
+++ notes-api/routes/notes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const {
     getNotes,
     postNote,
-    putNote
+    putNote,
+    deleteNote
 } = require("./notes/index.js");
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get('/', getNotes); // GET /notes
 router.post('/', postNote); // POST /notes
 router.put("/:id", putNote) // PUT /notes
+router.delete("/:id", deleteNote); // DELETE /notes
 
 module.exports = router;
