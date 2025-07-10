b88df4c689907f2bf4b4d70185c41ab30e847bdb
mchl <mchlginga@gmail.com>
2025-07-10 17:07:49 +0800

    add environment configuration

---
+++ notes-api-mongoose/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dev: "development",
+    port: process.env.PORT || 3000,
+    dbName: process.env.DB_NAME || "notesDB"
+};
---
+++ notes-api-mongoose/config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dev: "production",
+    port: process.env.PORT || 5000,
+    dbName: process.env.DB_NAME || "prod_notesDB"
+};
